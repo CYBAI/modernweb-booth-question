382a2619da618bb704af91f8d41e1560028c752b
CYBAI <cyb.ai.815@gmail.com>
2019-08-25 18:22:00 +0900

    Fix clippy warning

--- src/main.rs
+++ src/main.rs
@@ -14,27 +14,28 @@ fn main() {
         File::open("src/question.json").expect("Failed to reading file: question.json.");
     let subjects: Vec<Subject> = serde_json::from_reader(questions_file).unwrap();
 
-    let mut count = 0;
     let length = subjects.len();
-    for subject in &subjects {
-        count += 1;
+    for (count, subject) in subjects.iter().enumerate() {
         loop {
             println!(
                 "{}{}{}/{} 問題：{}{}",
                 color::Fg(color::Cyan),
                 style::Bold,
-                count,
+                count + 1,
                 length,
                 subject.question,
                 style::Reset
             );
+
             let mut guess = String::new();
             println!(
                 "{}請輸入答案：{}",
                 color::Fg(color::LightBlack),
                 style::Reset
             );
+
             io::stdin().read_line(&mut guess).expect("請輸入一些文字");
+
             if guess.trim().to_lowercase() == subject.answer {
                 println!(
                     "{}答對了，你好棒 owo{}",
